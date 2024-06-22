597c88f6b64e9d5977f674ed7edc33877ed7f84b
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-22 15:46:27 +0300

    hide cast-related recipes in jei

--- .minecraft/kubejs/client_scripts/tooltip/jei/hide/cast.js
+++ .minecraft/kubejs/client_scripts/tooltip/jei/hide/cast.js
@@ -1,5 +1,18 @@
+
+// Hide all cast-related recipes
 JEIEvents.removeRecipes(event => {
-    event.remove("kubejs:crafting_shapeless", [
-        "kubejs:kjs/ceramic_ingot_cast"
-    ]);
-})
+    global.casts.forEach(cast => {
+        event.remove("minecraft:crafting", `kubejs:empty_cast_${cast.id}`);
+        global.moltenMetals.forEach(metal => {
+            if (metal.temp > cast.maxTemp) {
+                return;
+            };
+
+            metal.items.forEach(item => {
+                for (let i = 1; i <= Math.min(8, Math.floor(9 / item.count)); i++) {
+                    event.remove("minecraft:crafting", `kubejs:fill_cast_${cast.id}_${metal.id}_${item.count}${i}`);
+                };
+            });
+        });
+    });
+});
