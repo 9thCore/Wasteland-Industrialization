411d5459ebf06b327311a6eef07e15601287cc37
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-22 15:45:54 +0300

    slightly lower indentation with a guard clause

--- .minecraft/kubejs/server_scripts/recipe/vanilla/crafting/ingot_cast.js
+++ .minecraft/kubejs/server_scripts/recipe/vanilla/crafting/ingot_cast.js
@@ -1,36 +1,38 @@
 ServerEvents.recipes(event => {
     global.casts.forEach(cast => {
         global.moltenMetals.forEach(metal => {
-            if (metal.temp <= cast.maxTemp) {
-                metal.items.forEach(item => {
-                    for (let i = 1; i <= Math.min(8, Math.floor(9 / item.count)); i++) {
-                        (function(index) {
-                            event.shapeless(
+            if (metal.temp > cast.maxTemp) {
+                return;
+            }
+            
+            metal.items.forEach(item => {
+                for (let i = 1; i <= Math.min(8, Math.floor(9 / item.count)); i++) {
+                    (function(index) {
+                        event.shapeless(
+                            `kubejs:${cast.id}_ingot_cast`,
+                            [
                                 `kubejs:${cast.id}_ingot_cast`,
-                                [
-                                    `kubejs:${cast.id}_ingot_cast`,
-                                    `${i}x ${item.id}`
-                                ]
-                            )
-                            .id(`kubejs:fill_cast_${cast.id}_${metal.id}_${item.count}${i}`)
-                            .modifyResult((grid, result) => {
-                                let nbt = grid.find(`kubejs:${cast.id}_ingot_cast`).getNbt();
-                                nbt = nbt ? nbt : global.getCastNBT();
-                                nbt.nugget_count += index * item.count;
-                                if (nbt.nugget_count > 9) {
-                                    return Item.of("minecraft:air");
-                                };
-                                if (nbt.metal_inside && nbt.metal_inside != metal.insideCastName) {
-                                    return Item.of("minecraft:air");
-                                };
-                                nbt.metal_inside = metal.insideCastName;
-                                result.setNbt(nbt);
-                                return result;
-                            });
-                        })(i);
-                    };
-                });
-            };
+                                `${i}x ${item.id}`
+                            ]
+                        )
+                        .id(`kubejs:fill_cast_${cast.id}_${metal.id}_${item.count}${i}`)
+                        .modifyResult((grid, result) => {
+                            let nbt = grid.find(`kubejs:${cast.id}_ingot_cast`).getNbt();
+                            nbt = nbt ? nbt : global.getCastNBT();
+                            nbt.nugget_count += index * item.count;
+                            if (nbt.nugget_count > 9) {
+                                return Item.of("minecraft:air");
+                            };
+                            if (nbt.metal_inside && nbt.metal_inside != metal.insideCastName) {
+                                return Item.of("minecraft:air");
+                            };
+                            nbt.metal_inside = metal.insideCastName;
+                            result.setNbt(nbt);
+                            return result;
+                        });
+                    })(i);
+                };
+            });
         });
 
         event.shapeless(`kubejs:${cast.id}_ingot_cast`, `kubejs:${cast.id}_ingot_cast`);
