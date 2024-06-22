79cbe211aebf1b02bc54cfd181e77be0b93b0ed0
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-22 18:28:53 +0300

    Create "fake" JEI recipes for casts
    actually real recipes that can't be crafted but its fine LOL

--- .minecraft/kubejs/server_scripts/recipe/vanilla/crafting/ingot_cast.js
+++ .minecraft/kubejs/server_scripts/recipe/vanilla/crafting/ingot_cast.js
@@ -5,6 +5,7 @@ ServerEvents.recipes(event => {
                 return;
             }
             
+            let j = 0;
             metal.items.forEach(item => {
                 for (let i = 1; i <= Math.min(8, Math.floor(9 / item.count)); i++) {
                     (function(index) {
@@ -32,6 +33,24 @@ ServerEvents.recipes(event => {
                         });
                     })(i);
                 };
+
+                // Fake recipe
+                event.shapeless(
+                    Item.of(`kubejs:${cast.id}_ingot_cast`)
+                    .withNBT({
+                        metal_inside: metal.insideCastName,
+                        nugget_count: item.count
+                    })
+                    .strongNBT(),
+                    [
+                        `kubejs:${cast.id}_ingot_cast`,
+                        `1x ${item.id}`
+                    ]
+                )
+                .id(`kubejs:cast_fill_hint_${cast.id}_${metal.id}_${j++}`)
+                .modifyResult(_ => {
+                    return Item.of("mineraft:air"); // Never return a real item
+                });
             });
         });
 
@@ -39,5 +58,13 @@ ServerEvents.recipes(event => {
             `kubejs:${cast.id}_ingot_cast`,
             `kubejs:${cast.id}_ingot_cast`
         ).id(`kubejs:empty_cast_${cast.id}`);
+
+        event.shapeless(
+            `kubejs:${cast.id}_ingot_cast`,
+            Item.of(`kubejs:${cast.id}_ingot_cast`).withNBT({
+                metal_inside: "any metal",
+                nugget_count: "?"
+            }).strongNBT()
+        ).id(`kubejs:cast_empty_hint_${cast.id}`);
     });
 })
