bb28321e65e771606364c7b9867fd61e0f1d318c
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-22 22:31:37 +0300

    add pebble on stone behaviour

---
+++ .minecraft/kubejs/server_scripts/recipe/lychee/item_application/stone_pebble.js
@@ -0,0 +1,43 @@
+import { RecipePost } from "../post";
+import { ItemApplicationRecipe } from "./recipe_type"
+
+ServerEvents.recipes(event => {
+    ItemApplicationRecipe.register(event, "kubejs:pebble_sharp", "minecraft:stone",
+        [
+            RecipePost.preventDefault(),
+            RecipePost.random(1, 1, [
+                RecipePost.break(),
+                RecipePost.break(),
+                RecipePost.break(),
+                RecipePost.break(),
+                RecipePost.break(),
+                RecipePost.break(),
+                RecipePost.break(),
+                RecipePost.break(),
+                RecipePost.break(),
+                RecipePost.break(),
+                RecipePost.dropItem("kubejs:pebble_chiseled"),
+                RecipePost.dropItem("kubejs:pebble"),
+                RecipePost.dropItem("kubejs:pebble_chiseled"),
+                RecipePost.dropItem("2x kubejs:pebble"),
+                RecipePost.dropItem("6x kubejs:pebble")
+            ]),
+            RecipePost.placeBlock(0, 0, 0, "minecraft:cobblestone")
+        ]
+    );
+    
+    ItemApplicationRecipe.register(event, "kubejs:pebble_sharp", "minecraft:cobblestone",
+        [
+            RecipePost.preventDefault(),
+            RecipePost.random(1, 1, [
+                RecipePost.break(),
+                RecipePost.break(),
+                RecipePost.dropItem("2x kubejs:pebble_sharp"),
+                RecipePost.dropItem("kubejs:pebble_chiseled"),
+                RecipePost.dropItem("3x kubejs:pebble_sharp"),
+                RecipePost.dropItem("5x kubejs:pebble_chiseled"),
+            ]),
+            RecipePost.destroyBlock()
+        ]
+    );
+});
