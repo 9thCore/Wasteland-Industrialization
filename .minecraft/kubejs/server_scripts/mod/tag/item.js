28bc48cb6d1414ae356541807f98056febcadf12
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-23 10:22:47 +0300

    add all pebbles to tag

--- .minecraft/kubejs/server_scripts/mod/tag/item.js
+++ .minecraft/kubejs/server_scripts/mod/tag/item.js
@@ -1,5 +1,11 @@
 ServerEvents.tags('item', event => {
 	event.add("kubejs:casts", [
 		"kubejs:ceramic_ingot_cast"
-	])
-})
+	]);
+
+	event.add("kubejs:pebbles", [
+		"kubejs:pebble",
+		"kubejs:pebble_chiseled",
+		"kubejs:pebble_sharp"
+	]);
+});
