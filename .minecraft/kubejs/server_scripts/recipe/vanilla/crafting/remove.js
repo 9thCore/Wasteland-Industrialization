cde9ca61b57b7bdfe3d2bb801d3bdcccc2609ea6
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-23 17:08:05 +0300

    remove recipes for wooden + stone tools

---
+++ .minecraft/kubejs/server_scripts/recipe/vanilla/crafting/remove.js
@@ -0,0 +1,13 @@
+ServerEvents.recipes(event => {
+    event.remove("minecraft:wooden_axe");
+    event.remove("minecraft:wooden_sword");
+    event.remove("minecraft:wooden_pickaxe");
+    event.remove("minecraft:wooden_shovel");
+    event.remove("minecraft:wooden_hoe");
+    
+    event.remove("minecraft:stone_axe");
+    event.remove("minecraft:stone_sword");
+    event.remove("minecraft:stone_pickaxe");
+    event.remove("minecraft:stone_shovel");
+    event.remove("minecraft:stone_hoe");
+});
