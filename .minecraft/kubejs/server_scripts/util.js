33b69b9cfcdd32b669efaace293f73598da62385
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-23 09:01:24 +0300

    remove unused code

--- .minecraft/kubejs/server_scripts/util.js
+++ .minecraft/kubejs/server_scripts/util.js
@@ -1,25 +1,5 @@
 const countItemRegex = /^(\d+)x (.+)$/;
 
-// Code from Swedz, posted at https://github.com/KubeJS-Mods/KubeJS/issues/602
-// Used to get around race conditions involving tags?
-const forEachItemMatchingRegex = (regex, callback) =>
-{
-  let regexes = constructArrayIfNeeded(regex);
-  regexes.map(r => Ingredient.of(r).itemIds).forEach(items => items.forEach(item => callback(item)));
-}
-
-// Function that's called to add something to a tag
-// If it's a regex, does workaround present above - otherwise, normal behaviour
-function addThingToTag(event, thing, tag) {
-	if (thing instanceof RegExp) {
-		forEachItemMatchingRegex(thing, item => {
-			event.add(tag, item)
-		})
-	} else {
-		event.add(tag, thing)
-	}
-}
-
 function arbitraryRangeRandom(min, max) {
 	return Math.random() * (max - min) + min;
 }
