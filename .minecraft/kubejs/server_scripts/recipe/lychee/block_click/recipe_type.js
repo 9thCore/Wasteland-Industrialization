21343b4e6c75ef58eec264884a13e51c108a9db0
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-23 10:39:52 +0300

    allow two items in ItemApplication and BlockClick
    mainhand and offhand

--- .minecraft/kubejs/server_scripts/recipe/lychee/block_click/recipe_type.js
+++ .minecraft/kubejs/server_scripts/recipe/lychee/block_click/recipe_type.js
@@ -5,18 +5,22 @@ export let BlockClickRecipe = {};
 /**
  * 
  * @param {Internal.RecipesEventJS} event 
- * @param {String} item
+ * @param {String|String[]} items
  * @param {Object} blockPredicate
  * @param {Object} post
  */
-BlockClickRecipe.register = function(event, item, blockPredicate, post) {
-    const [count, name] = stripCountFromItem(item);
+BlockClickRecipe.register = function(event, items, blockPredicate, post) {
+    let items_in = [];
+    items = Array.isArray(items) ? items : [items];
+    items.forEach(name => {
+        items_in.push({
+            item: name
+        });
+    });
+
     event.custom({
         type: "lychee:block_clicking",
-        item_in: {
-            item: name,
-            count: count
-        },
+        item_in: items_in,
         block_in: blockPredicate,
         post: post
     });
--- .minecraft/kubejs/server_scripts/recipe/lychee/item_application/recipe_type.js
+++ .minecraft/kubejs/server_scripts/recipe/lychee/item_application/recipe_type.js
@@ -5,18 +5,22 @@ export let ItemApplicationRecipe = {};
 /**
  * 
  * @param {Internal.RecipesEventJS} event 
- * @param {String} item
+ * @param {String|String[]} items
  * @param {Object} blockPredicate
  * @param {Object} post
  */
-ItemApplicationRecipe.register = function(event, item, blockPredicate, post) {
-    const [count, name] = stripCountFromItem(item);
+ItemApplicationRecipe.register = function(event, items, blockPredicate, post) {
+    let items_in = [];
+    items = Array.isArray(items) ? items : [items];
+    items.forEach(name => {
+        items_in.push({
+            item: name
+        });
+    });
+
     event.custom({
         type: "lychee:block_interacting",
-        item_in: {
-            item: name,
-            count: count
-        },
+        item_in: items_in,
         block_in: blockPredicate,
         post: post
     });
