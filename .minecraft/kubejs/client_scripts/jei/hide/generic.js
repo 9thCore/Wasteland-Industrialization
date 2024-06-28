1ff92b1dfa660e04a91c402bb92e063383ef4541
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-28 17:34:56 +0300

    remove crossroads dusts

--- .minecraft/kubejs/client_scripts/jei/hide/generic.js
+++ .minecraft/kubejs/client_scripts/jei/hide/generic.js
@@ -14,7 +14,14 @@ JEIEvents.hideItems(event => {
     event.hide('crossroads:nugget_tin');
     event.hide('crossroads:raw_tin');
     event.hide('crossroads:block_tin');
+    event.hide('crossroads:ore_tin');
+    event.hide('crossroads:ore_tin_deep');
     
+    event.hide('crossroads:dust_iron');
+    event.hide('crossroads:dust_gold');
+    event.hide('crossroads:dust_copper');
+    event.hide('crossroads:dust_tin');
+
     event.hide('electrodynamics:geariron');
     event.hide('electrodynamics:gearcopper');
     event.hide('electrodynamics:geartin');
