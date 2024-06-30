e11a7e8e1aee7c096dd53426490ca00b743ac744
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-30 22:09:00 +0300

    remove smelting recipes

---
+++ .minecraft/kubejs/server_scripts/recipe/vanilla/smelting/filter.js
@@ -0,0 +1,17 @@
+// priority: 1000
+
+export const Filters = {
+    generic: function(type, output) {
+        return {type: type, output: output};
+    },
+    smelting: function(output) {
+        return this.generic("minecraft:smelting", output);
+    },
+
+    blasting:  function(output) {
+        return this.generic("minecraft:blasting", output);
+    },
+    either: function(output) {
+        return [this.smelting(output), this.blasting(output)];
+    }
+};
