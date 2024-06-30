154071b7f2f10884a3236a2b491c97b80b8b5481
9thCore <38700349+9thCore@users.noreply.github.com>
2024-06-30 22:08:39 +0300

    remove more metal recipes

--- .minecraft/kubejs/server_scripts/recipe/vanilla/crafting/remove/metal.js
+++ .minecraft/kubejs/server_scripts/recipe/vanilla/crafting/remove/metal.js
@@ -1,30 +1,28 @@
-(function() {
-    ServerEvents.recipes(event => {
-        event.remove("minecraft:gold_ingot_from_nuggets");
-        event.remove("minecraft:gold_ingot_from_gold_block");
-        event.remove("minecraft:gold_nugget");
-        event.remove("minecraft:gold_block");
-        
-        event.remove("minecraft:iron_ingot_from_nuggets");
-        event.remove("minecraft:iron_ingot_from_iron_block");
-        event.remove("minecraft:iron_nugget");
-        event.remove("minecraft:iron_block");
-        
-        event.remove("minecraft:netherite_ingot_from_netherite_block");
-        event.remove("minecraft:netherite_block");
-        
-        event.remove("minecraft:copper_ingot");
-        event.remove("minecraft:copper_ingot_from_waxed_copper_block");
-        event.remove("crossroads:base_materials/copper_ingot_nugg");
-        event.remove("electrodynamics:copper_nuggets_to_copper_ingot");
-        event.remove("crossroads:base_materials/copper_nugget");
-        event.remove("minecraft:copper_block");
-        
-        event.remove("crossroads:base_materials/bronze_alloy_nugg");
+ServerEvents.recipes(event => {
+    event.remove("minecraft:gold_ingot_from_nuggets");
+    event.remove("minecraft:gold_ingot_from_gold_block");
+    event.remove("minecraft:gold_nugget");
+    event.remove("minecraft:gold_block");
+    
+    event.remove("minecraft:iron_ingot_from_nuggets");
+    event.remove("minecraft:iron_ingot_from_iron_block");
+    event.remove("minecraft:iron_nugget");
+    event.remove("minecraft:iron_block");
+    
+    event.remove("minecraft:netherite_ingot_from_netherite_block");
+    event.remove("minecraft:netherite_block");
+    
+    event.remove("minecraft:copper_ingot");
+    event.remove("minecraft:copper_ingot_from_waxed_copper_block");
+    event.remove("crossroads:base_materials/copper_ingot_nugg");
+    event.remove("electrodynamics:copper_nuggets_to_copper_ingot");
+    event.remove("crossroads:base_materials/copper_nugget");
+    event.remove("minecraft:copper_block");
+    
+    event.remove("crossroads:base_materials/bronze_alloy_nugg");
 
-        event.remove("crossroads:base_materials/copshowium_ingot_nugg");
-        event.remove("crossroads:base_materials/copshowium_nugget");
-        event.remove("crossroads:base_materials/copshowium_ingot_block");
-        event.remove("crossroads:base_materials/copshowium_block");
-    });
-})();
+    event.remove("crossroads:base_materials/copshowium_ingot_nugg");
+    event.remove("crossroads:base_materials/copshowium_nugget");
+    event.remove("crossroads:base_materials/copshowium_ingot_block");
+    event.remove("crossroads:base_materials/copshowium_block");
+});
