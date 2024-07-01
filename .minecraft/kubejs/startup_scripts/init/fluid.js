3f4ecb6a83b08249bf81ba324d4afa42c3ef17f2
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-01 21:03:28 +0300

    remove unused commented fluid registration

--- .minecraft/kubejs/startup_scripts/init/fluid.js
+++ .minecraft/kubejs/startup_scripts/init/fluid.js
@@ -1,8 +1,3 @@
 StartupEvents.registry('fluid', event => {
-    /*
-    event.create('molten_bronze')
-    .thickTexture(0xEC9549)
-    .bucketColor(0xEC9549)
-    .displayName('Molten Bronze')
-    */
+    // dummy
 })
