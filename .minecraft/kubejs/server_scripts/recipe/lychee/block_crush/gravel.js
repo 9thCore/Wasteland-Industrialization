6f91bcaf99ed85194fa5182eb4866d2283c10da8
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-02 22:08:30 +0300

    add cobblestone -> gravel crushing
    - I am proud to announce that anvil crushing is FULLY AUTOMATABLE as soon as Redstone is reached (when iron tools are reached and ores give the entire ore instead of bits)
    - successfully devised a machine that does this, with great efficiency
    - if I can do it, surely the players can, too! smiley face
    - nothing but vanilla and Essentials items used
    - the player may, instead, choose to go the (much simpler) way of the Crossroads Millstone, but at the cost of not getting the ore drops (the millstone is a bit *too* efficient at crushing)

---
+++ .minecraft/kubejs/server_scripts/recipe/lychee/block_crush/gravel.js
@@ -0,0 +1,24 @@
+import { RecipePost } from "../post";
+import { BlockCrushingRecipe } from "./recipe_type";
+
+ServerEvents.recipes(event => {
+    BlockCrushingRecipe.register(event, "minecraft:cobblestone", null, null, [
+        RecipePost.dropItem("minecraft:gravel"),
+        RecipePost.random(1, 2, [
+            RecipePost.break(),
+            RecipePost.break(),
+            RecipePost.break(),
+            RecipePost.break(),
+            RecipePost.dropItem("kubejs:iron_bit"),
+            RecipePost.dropItem("2x kubejs:iron_bit"),
+            RecipePost.dropItem("kubejs:coal_bit"),
+            RecipePost.dropItem("kubejs:coal_bit"),
+            RecipePost.dropItem("2x kubejs:coal_bit"),
+            RecipePost.dropItem("3x kubejs:coal_bit"),
+            RecipePost.dropItem("2x kubejs:copper_bit"),
+            RecipePost.dropItem("3x kubejs:copper_bit"),
+            RecipePost.dropItem("3x kubejs:copper_bit"),
+            RecipePost.dropItem("4x kubejs:copper_bit")
+        ])
+    ]);
+});
---
+++ .minecraft/kubejs/server_scripts/recipe/lychee/block_crush/recipe_type.js
@@ -0,0 +1,36 @@
+// priority: 100
+
+export let BlockCrushingRecipe = {};
+
+/**
+ * 
+ * @param {Internal.RecipesEventJS} event 
+ * @param {String|String[]} [items]
+ * @param {Object} [fallingBlockPredicate]
+ * @param {Object} [landingBlockPredicate]
+ * @param {Object} post
+ */
+BlockCrushingRecipe.register = function(event, items, fallingBlockPredicate, landingBlockPredicate, post) {
+    let items_in = [];
+    items = Array.isArray(items) ? items : [items];
+    items.forEach(name => {
+        items_in.push({
+            item: name
+        });
+    });
+
+    const recipe = {
+        type: "lychee:block_crushing",
+        item_in: items_in,
+        post: post
+    };
+    
+    if (fallingBlockPredicate) {
+        recipe.falling_block = fallingBlockPredicate;
+    };
+    if (landingBlockPredicate) {
+        recipe.landing_block = landingBlockPredicate;
+    };
+
+    event.custom(recipe);
+};
