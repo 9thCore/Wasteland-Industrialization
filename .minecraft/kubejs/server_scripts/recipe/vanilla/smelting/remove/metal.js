a9a9c54a33817dc06907f99a65fb600c6fa2ea20
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-08 20:48:34 +0300

    remove nuggets from smelting as well

--- .minecraft/kubejs/server_scripts/recipe/vanilla/smelting/remove/metal.js
+++ .minecraft/kubejs/server_scripts/recipe/vanilla/smelting/remove/metal.js
@@ -1,4 +1,5 @@
 ServerEvents.recipes(event => {
     event.remove(Filters.smelting("#forge:ingots"));
     event.remove(Filters.smelting("#forge:gems"));
+    event.remove(Filters.smelting("#forge:nuggets"));
 });
