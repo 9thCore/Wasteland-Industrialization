fb0242dfd5370a79d1530a6b4d2a85814c04add4
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-08 21:08:05 +0300

    add ingot or ore -> dust recipes via anvil crushing
    - bad ratio, about a 3:1
    - best you can get until the millstone (or electrical grinder)

---
+++ .minecraft/kubejs/server_scripts/recipe/lychee/block_crush/dust.js
@@ -0,0 +1,33 @@
+// priority: -100
+
+(function() {
+    /**
+     * 
+     * @param {String[]} inputs 
+     * @param {String} output 
+     */
+    const helper = function(event, inputs, output) {
+        inputs = Array.isArray(inputs) ? inputs : [inputs];
+        inputs.forEach(input => {
+            LycheeUtils.register(
+                event, 
+                BlockCrushingRecipe.construct(input),
+                RecipePost.random(1, 1, [
+                    RecipePost.break(),
+                    RecipePost.break(),
+                    RecipePost.dropItem(output)
+                ])
+            );
+        });
+    };
+
+    ServerEvents.recipes(event => {
+        helper(event, ["minecraft:iron_ingot", "minecraft:raw_iron"], "electrodynamics:dustiron");
+        helper(event, ["minecraft:gold_ingot", "minecraft:raw_gold"], "electrodynamics:dustgold");
+        helper(event, ["minecraft:copper_ingot", "minecraft:raw_copper"], "electrodynamics:dustcopper");
+        helper(event, ["electrodynamics:ingottin", "electrodynamics:raworetin"], "electrodynamics:dusttin");
+        helper(event, ["electrodynamics:ingotsilver", "electrodynamics:raworesilver"], "electrodynamics:dustsilver");
+        helper(event, "electrodynamics:ingotsteel", "electrodynamics:duststeel");
+        helper(event, ["electrodynamics:ingotlead", "electrodynamics:raworelead"], "electrodynamics:dustlead");
+    });
+})();
