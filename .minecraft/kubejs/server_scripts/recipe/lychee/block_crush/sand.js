4a2d6cb546cfa43e704beebe7ce9e693495fa0ed
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-17 13:12:53 +0300

    remove ore bits from gravel -> sand recipe

--- .minecraft/kubejs/server_scripts/recipe/lychee/block_crush/sand.js
+++ .minecraft/kubejs/server_scripts/recipe/lychee/block_crush/sand.js
@@ -2,22 +2,6 @@ ServerEvents.recipes(event => {
     LycheeUtils.register(
         event, 
         BlockCrushingRecipe.construct("minecraft:gravel"),
-        [
-            RecipePost.dropItem("minecraft:sand"),
-            RecipePost.random(1, 1, [
-                RecipePost.break(),
-                RecipePost.break(),
-                RecipePost.break(),
-                RecipePost.break(),
-                RecipePost.break(),
-                RecipePost.break(),
-                RecipePost.break(),
-                RecipePost.break(),
-                RecipePost.dropItem("kubejs:iron_bit"),
-                RecipePost.dropItem("kubejs:coal_bit"),
-                RecipePost.dropItem("kubejs:coal_bit"),
-                RecipePost.dropItem("kubejs:copper_bit")
-            ])
-        ]
+        RecipePost.dropItem("minecraft:sand")
     );
 });
