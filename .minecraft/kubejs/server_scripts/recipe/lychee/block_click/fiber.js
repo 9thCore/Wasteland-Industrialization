5aea6c98237346cc58ea64623cc90fe55544f5ce
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-18 18:09:46 +0300

    add swords as dead bush cutters for fiber
    no reason besides sharp thing hehe

--- .minecraft/kubejs/server_scripts/recipe/lychee/block_click/fiber.js
+++ .minecraft/kubejs/server_scripts/recipe/lychee/block_click/fiber.js
@@ -1,19 +1,53 @@
-ServerEvents.recipes(event => {
-    LycheeUtils.register(
-        event,
-        BlockClickRecipe.construct("kubejs:pebble_sharp", "minecraft:dead_bush"),
-        [
-            RecipePost.preventDefault(),
-            RecipePost.destroyBlock(),
-            RecipePost.dropItem("kubejs:dry_fiber"),
-            RecipePost.random(1, 1, [
-                RecipePost.break().setWeight(5),
-                RecipePost.dropItem("kubejs:dry_fiber")
-            ]),
-            RecipePost.random(1, 1, [
-                RecipePost.break().setWeight(8),
-                RecipePost.dropItem("kubejs:dry_fiber")
-            ])
-        ]
-    );
-});
+(function() {
+    const swords = [
+        "minecraft:iron_sword",
+        "minecraft:diamond_sword",
+        "minecraft:netherite_sword"
+    ]
+
+    ServerEvents.recipes(event => {
+        LycheeUtils.register(
+            event,
+            BlockClickRecipe.construct("kubejs:pebble_sharp", "minecraft:dead_bush"),
+            [
+                RecipePost.preventDefault(),
+                RecipePost.destroyBlock(),
+                RecipePost.dropItem("kubejs:dry_fiber"),
+                RecipePost.random(1, 1, [
+                    RecipePost.break().setWeight(5),
+                    RecipePost.dropItem("kubejs:dry_fiber")
+                ]),
+                RecipePost.random(1, 1, [
+                    RecipePost.break().setWeight(8),
+                    RecipePost.dropItem("kubejs:dry_fiber")
+                ])
+            ]
+        );
+        
+        swords.forEach(sword => {
+            LycheeUtils.register(
+                event,
+                BlockClickRecipe.construct(sword, "minecraft:dead_bush"),
+                [
+                    RecipePost.preventDefault(),
+                    RecipePost.damageItem(),
+                    RecipePost.destroyBlock(),
+                    RecipePost.dropItem("kubejs:dry_fiber"),
+                    RecipePost.random(1, 1, [
+                        RecipePost.break().setWeight(3),
+                        RecipePost.dropItem("kubejs:dry_fiber")
+                    ]),
+                    RecipePost.random(1, 1, [
+                        RecipePost.break().setWeight(6),
+                        RecipePost.dropItem("kubejs:dry_fiber")
+                    ]),
+                    RecipePost.random(1, 1, [
+                        RecipePost.break().setWeight(9),
+                        RecipePost.dropItem("kubejs:dry_fiber")
+                    ])
+                ]
+            );
+        });
+        
+    });
+})();
