78418ec7d045e7c2e1bcb44dd0515cc2fc1ce40d
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-18 15:56:28 +0300

    fix canister tooltip to support all fluids

--- .minecraft/kubejs/client_scripts/jei/tooltip/canister.js
+++ .minecraft/kubejs/client_scripts/jei/tooltip/canister.js
@@ -1,15 +1,24 @@
-ItemEvents.tooltip(event => {
-    event.addAdvanced("kubejs:canister", (item, advanced, text) => {
-        const cap = item.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM);
-        cap.ifPresent(c => {
-            const stack = c.getFluidInTank(0);
-            if (stack.isEmpty()) {
-                return;
-            };
+(function() {
+    const specialKeys = {
+        "minecraft.water": "block.minecraft.water",
+        "minecraft.lava": "block.minecraft.lava",
+        "minecraft.flowing_milk": "fluid_type.minecraft.milk"
+    }
 
-            const id = stack.fluid.arch$registryName();
-            text.add(1, Text.translate(`fluid.${id.toLanguageKey()}`).darkGray());
-            text.add(1, Text.gray(`${stack.amount} mB / ${c.getTankCapacity(0)} mB`));
+    ItemEvents.tooltip(event => {
+        event.addAdvanced("kubejs:canister", (item, advanced, text) => {
+            const cap = item.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM);
+            cap.ifPresent(c => {
+                const stack = c.getFluidInTank(0);
+                if (stack.isEmpty()) {
+                    return;
+                };
+    
+                const id = stack.fluid.arch$registryName().toLanguageKey();
+                const key = specialKeys[id] != null ? specialKeys[id] : `fluid.${id}`;
+                text.add(1, Text.translate(key).darkGray());
+                text.add(1, Text.gray(`${stack.amount} mB / ${c.getTankCapacity(0)} mB`));
+            });
         });
     });
-});
+})();
