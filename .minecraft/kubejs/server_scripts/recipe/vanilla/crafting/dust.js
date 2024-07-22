0c0e6bf2e1f1f91481e9b04e0b181cf563521ce2
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-22 15:21:24 +0300

    add steel dust recipe
    1:1 iron:steel :)

--- .minecraft/kubejs/server_scripts/recipe/vanilla/crafting/dust.js
+++ .minecraft/kubejs/server_scripts/recipe/vanilla/crafting/dust.js
@@ -3,4 +3,12 @@ ServerEvents.recipes(event => {
     event.replaceOutput({mod: "crossroads"}, "crossroads:dust_gold", "electrodynamics:dustgold");
     event.replaceOutput({mod: "crossroads"}, "crossroads:dust_copper", "electrodynamics:dustcopper");
     event.replaceOutput({mod: "crossroads"}, "crossroads:dust_tin", "electrodynamics:dusttin");
+
+    event.shapeless(
+        Item.of("electrodynamics:duststeel", 3),
+        [
+            Item.of("electrodynamics:dustiron", 3),
+            "kubejs:coke_dust"
+        ]
+    );
 });
