f90c3f1c3eea0f85b70831bfe1d9b902b19d3fd6
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-23 20:35:42 +0300

    debug info

--- .minecraft/kubejs/client_scripts/jei/hide/generic.js
+++ .minecraft/kubejs/client_scripts/jei/hide/generic.js
@@ -1,14 +1,22 @@
+// priority: 100
+
 JEIEvents.hideItems(event => {
+    console.log("Started hide event"); // 
+
     event.hide('kubejs:pebble_block');
     event.hide('ftbquests:barrier');
     event.hide('ftbquests:stage_barrier');
     event.hide('ftbquests:missing_item');
     event.hide('ftbquests:custom_icon');
+
+    console.log("Checkpoint 1"); // 
     
     event.hide('crossroads:nugget_copper');
     
     event.hide('crossroads:ingot_bronze');
     event.hide('crossroads:block_bronze');
+
+    console.log("Checkpoint 2"); // 
 
     event.hide('crossroads:ingot_tin');
     event.hide('crossroads:nugget_tin');
@@ -23,15 +31,21 @@ JEIEvents.hideItems(event => {
     event.hide('crossroads:dust_copper');
     event.hide('crossroads:dust_tin');
 
+    console.log("Checkpoint 3"); // 
+
     event.hide('electrodynamics:geariron');
     event.hide('electrodynamics:gearcopper');
     event.hide('electrodynamics:geartin');
     event.hide('electrodynamics:gearsteel');
     event.hide('electrodynamics:gearbronze');
+
+    console.log("Checkpoint 4"); // 
     
     event.hide('essentials:auto_crafter');
     event.hide('crossroads:dust_salt');
     event.hide('crossroads:sulfur');
+
+    console.log("Checkpoint 5"); // 
 
     event.hide('assemblyline:conveyorbelt');
     event.hide('assemblyline:sorterbelt');
@@ -41,4 +55,6 @@ JEIEvents.hideItems(event => {
     event.hide('assemblyline:rancher');
     event.hide('assemblyline:mobgrinder');
     event.hide('assemblyline:farmer');
+
+    console.log("Checkpoint 6"); // 
 })
