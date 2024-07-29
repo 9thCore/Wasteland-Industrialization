3630bea994e6e9ccac33daba1c161eab9039c4fe
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-29 21:26:25 +0300

    set max stack size for bottled clouds and honey bottles to 1

--- .minecraft/kubejs/startup_scripts/modification/item/generic.js
+++ .minecraft/kubejs/startup_scripts/modification/item/generic.js
@@ -2,4 +2,8 @@ ItemEvents.modification(event => {
     event.modify("electrodynamics:coalcoke", c => {
         c.setBurnTime(3200);
     });
+
+    event.modify(["quark:bottled_cloud", "minecraft:honey_bottle"], c => {
+        c.setMaxStackSize(1);
+    });
 });
