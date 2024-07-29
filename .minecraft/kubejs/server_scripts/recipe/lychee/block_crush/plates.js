c2779b7581fe204647c86eb8d7fff41aa9cef4d9
9thCore <38700349+9thCore@users.noreply.github.com>
2024-07-29 17:21:19 +0300

    lower plate:ingot from 9:2 to 3:1
    it was going to happen at some point

--- .minecraft/kubejs/server_scripts/recipe/lychee/block_crush/plates.js
+++ .minecraft/kubejs/server_scripts/recipe/lychee/block_crush/plates.js
@@ -8,12 +8,12 @@
     };
 
     ServerEvents.recipes(event => {
-        Register(event, "minecraft:iron_block", "2x electrodynamics:plateiron");
-        Register(event, "minecraft:copper_block", "2x electrodynamics:platecopper");
-        Register(event, "electrodynamics:resourceblocksteel", "2x electrodynamics:platesteel");
-        Register(event, "electrodynamics:resourceblocklead", "2x electrodynamics:platelead");
-        Register(event, "electrodynamics:resourceblockbronze", "2x electrodynamics:platebronze");
-        Register(event, "4x electrodynamics:ceramiccooked", "electrodynamics:ceramicplate");
-        Register(event, "minecraft:diamond_block", "2x kubejs:diamond_plate");
+        Register(event, "minecraft:iron_block", "3x electrodynamics:plateiron");
+        Register(event, "minecraft:copper_block", "3x electrodynamics:platecopper");
+        Register(event, "electrodynamics:resourceblocksteel", "3x electrodynamics:platesteel");
+        Register(event, "electrodynamics:resourceblocklead", "3x electrodynamics:platelead");
+        Register(event, "electrodynamics:resourceblockbronze", "3x electrodynamics:platebronze");
+        Register(event, "3x electrodynamics:ceramiccooked", "electrodynamics:ceramicplate");
+        Register(event, "minecraft:diamond_block", "3x kubejs:diamond_plate");
     });
 })();
